115a527efc9ca9f463e5434569255630d05c0b16
Matthew Mongeau <matt@toastyapps.com>
2010-08-27 23:13:06 -0400

    Can't vendor worth a shit

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-var socketIO = require(__dirname+"/socketIO").socketIO,
-    app = require('express').createServer();
+// var socketIO = require(__dirname+"/socketIO").socketIO,
+//     app = require('express').createServer();
+//
+// app.get('/', function (req, res) {
+//   res.send('oh hai');
+// });
+//
+// var port = process.argv.length == 3 ? parseInt(process.argv[2]) : 80
+// app.listen(port);
+//
+var http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('oh hai');
-});
+var server = http.createServer(function (req, res) {
+      res.writeHead(200, { "Content-Type": "text/plain" })
+        res.end("Hello world\n");
+      });
 
-var port = process.argv.length == 3 ? parseInt(process.argv[2]) : 80
-app.listen(port);
+    server.listen(80);
