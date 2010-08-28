ccb0f11798c89d1c8bfd0217b2d82a8bedba7962
Matthew Mongeau <matt@toastyapps.com>
2010-08-28 11:41:58 -0400

    Check for invalid requests

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var express = require('express'),
     app = express.createServer(),
     io = require('socket.io');
     server = http.createServer(),
-    socket = io.listen(server);
+    socket = io.listen(server),
+    json = JSON.stringify,
+    log = sys.puts,
+    actions = [];
 
 app.configure(function() {
   app.use(express.staticProvider(__dirname + '/public'));
@@ -18,8 +21,25 @@ app.listen(port, '0.0.0.0');
 
 server.listen(8024);
 
+function invalidRequest(request) {
+  return actions.indexOf(request.action) == -1;
+}
+
 socket.on('connection', function(client) {
   client.on('message', function(message) {
+    try {
+      request = JSON.parse(message.replace('<', '&lt;').replace('>', '&gt;'));
+    } catch (SyntaxError) {
+      log('Invalid JSON:');
+      log(message);
+      return false;
+    }
+
+    if(invalidRequest(request)) {
+      log('Invalid request:' + "\n" + message);
+      return false;
+    }
+
     console.log(message);
   });
 });
