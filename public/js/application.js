9edc186bd8d4d5253246acb9e1145a94b2b8be38
Matthew Mongeau <matt@toastyapps.com>
2010-08-29 16:05:14 -0400

    Start not save

--- public/js/application.js
+++ public/js/application.js
@@ -3,4 +3,4 @@ var callback = function() {
   Game.addEmptyDialogue("<h2>Waiting on an opponent...</h2>");
   Game.connect();
 }
-Game.displayHelp('Save', callback);
+Game.displayHelp('Start', callback);
